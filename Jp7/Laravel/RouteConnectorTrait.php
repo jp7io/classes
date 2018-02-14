8e19bdf907c1740d0c82d4cec868670f1282f786
Carlos Rodrigues <carlos@jp7.com.br>
2018-02-14 14:49:31 -0200

    Breaking Change / Rename mapWebRoutes() to map()

--- Jp7/Laravel/RouteConnectorTrait.php
+++ Jp7/Laravel/RouteConnectorTrait.php
@@ -3,52 +3,28 @@
 namespace Jp7\Laravel;
 
 use Illuminate\Routing\Router;
-use Jp7\Interadmin\DynamicLoader;
-use Jp7\Laravel\RouterFacade as r;
+use Jp7\Interadmin\RecordClassMap;
 
 trait RouteConnectorTrait
 {
     /**
-     * Define the "web" routes for the application.
+     * Define the routes for the application.
      *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @param \Illuminate\Routing\Router $router
+     * @return void
      */
-    protected function mapWebRoutes(Router $router)
+    public function map()
     {
-        if ($this->app->isDownForMaintenance()) {
-            return;
-        }
-        if (!DynamicLoader::isRegistered()) {
-            $this->showDbNotConnected();
+        if (env('SKIP_ROUTES')) {
             return;
         }
         // Clear Interadmin route map - allows route:cache to work
-        r::clearCache();
+        RouterFacade::clearCache();
 
         // Normal Laravel routing
-        $router->group([
-            'namespace' => $this->namespace, 'middleware' => 'web',
-        ], function ($router) {
-            require app_path('Http/routes.php');
-        });
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
 
         // Save new Interadmin route map
-        r::saveCache();
-    }
-
-    protected function showDbNotConnected()
-    {
-        $errorMsg = 'InterAdmin not connected. Possible causes:
-            1 - nano .env
-                => Check DB_HOST, DB_USERNAME, ...
-            2 - nano config/app.php
-                => Make sure InteradminServiceProvider is before RouteServiceProvider';
-        if (!\App::runningInConsole()) {
-            throw new \Exception($errorMsg);
-        }
-        // Exception is not thrown because artisan commands would stop working
-        echo '[Skipped routes] '.$errorMsg.PHP_EOL;
+        RouterFacade::saveCache();
     }
 }
