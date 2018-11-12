6730661cc058fe409867de0d1e7ce48e06454a8f
Carlos Rodrigues <carlos@jp7.com.br>
2018-11-12 18:02:53 -0200

    Update DontCacheOldInput.php

--- Jp7/Laravel/Middleware/DontCacheOldInput.php
+++ Jp7/Laravel/Middleware/DontCacheOldInput.php
@@ -5,8 +5,10 @@ namespace Jp7\Laravel\Middleware;
 use Closure;
 
 /**
- * Avoids old() from validation errors from being cached in pages with a form
- * Must be added after \Barryvdh\HttpCache\Middleware\CacheRequests::class in the Kernel
+ * Avoids old() form validation errors from being cached in pages with a form.
+ * Must be added after \Barryvdh\HttpCache\Middleware\CacheRequests::class in the Kernel.
+ * It was first added because https://www.abipe.org.br/ has a login form, but we can't remove the cache from the home page.
+ * Could have been solved with AJAX or an iframe, but we don't know how many other pages are affected by this.
  */
 class DontCacheOldInput
 {
