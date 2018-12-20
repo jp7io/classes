2fb287b6dd10caba778fd466ff2d8df5483c3b56
Carlos Rodrigues <carlos@jp7.com.br>
2018-12-20 19:34:53 -0200

    Update Jp7/HttpCache/ServiceProvider for Laravel 5.7

--- Jp7/HttpCache/ServiceProvider.php
+++ Jp7/HttpCache/ServiceProvider.php
@@ -3,26 +3,29 @@
 namespace Jp7\HttpCache;
 
 use Barryvdh\HttpCache\ServiceProvider as BaseServiceProvider;
+use Barryvdh\StackMiddleware\StackMiddleware;
+use Symfony\Component\HttpKernel\HttpCache\StoreInterface;
+use Symfony\Component\HttpKernel\HttpCache\Esi;
+use Symfony\Component\HttpKernel\HttpCache\Store as SymfonyStore;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function boot()
     {
         $app = $this->app;
-        $app['http_cache.store'] = $app->share(function ($app) {
+        $app->singleton(SymfonyStore::class, function ($app) {
             return new Store($app['http_cache.cache_dir']);
         });
 
-        $stack = app('Barryvdh\StackMiddleware\StackMiddleware');
-
-        $stack->bind(
-            'Jp7\HttpCache\CacheRequests',
-            HttpCacheExtension::class,
-            [
-                \App::make('http_cache.store'),
-                \App::make('http_cache.esi'),
-                \App::make('http_cache.options')
-            ]
+        $this->app->make(StackMiddleware::class)->bind(CacheRequests::class,
+            function($app) {
+              return new HttpCacheExtension(
+                  $app,
+                  $this->app->make(StoreInterface::class),
+                  $this->app->make(Esi::class),
+                  $this->app['http_cache.options']
+              );
+            }
         );
     }
 }
