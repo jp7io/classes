3b599d2dd5b3633885a5ba87a21acfafdf4816d0
João Pedro Barberino <42822732+joaopbarberino@users.noreply.github.com>
2020-01-27 12:45:42 -0300

    Fix string offset with curly braces deprecated in PHP 7.4

--- Jp7/Validator/Cpf.php
+++ Jp7/Validator/Cpf.php
@@ -14,11 +14,11 @@ class Cpf
         } else {
             for ($t = 9; $t < 11; $t++) {
                 for ($d = 0, $c = 0; $c < $t; $c++) {
-                    $d += $cpf{$c}
+                    $d += $cpf[$c]
                     * (($t + 1) - $c);
                 }
                 $d = ((10 * $d) % 11) % 10;
-                if ($cpf{$c} != $d) {
+                if ($cpf[$c] != $d) {
                     return false;
                 }
             }
